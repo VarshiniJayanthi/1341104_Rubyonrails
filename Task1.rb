785ddfd8fce60471b072f85a0c5602c0134f548b
VarshiniJayanthi <32540289+VarshiniJayanthi@users.noreply.github.com>
2017-10-16 17:38:34 +0530

    Add files via upload

---
+++ Task1.rb
@@ -0,0 +1,11 @@
+def no_of_laps(x, y)
+
+result = x.lcm(y)
+ 
+return [result/x , result/y]
+	
+ 
+end
+
+puts "#{no_of_laps(5, 3)}"
+
---
+++ Task5.rb
@@ -0,0 +1,33 @@
+def mix(s1, s2)
+	
+selection = ('a'..'z').to_a.select { |letter|
+ s1.count(letter) > 1 || s2.count(letter) > 1 
+}
+	
+selection.map! do |selection|
+	
+if s1.count(selection) > s2.count(selection)
+
+	"1:#{selection * s1.count(selection)}"
+	
+elsif s1.count(selection) < s2.count(selection)
+	
+        "2:#{selection * s2.count(selection)}"
+	
+else
+	
+   "=:#{selection * s1.count(selection)}"
+	
+end
+	
+end
+	
+selection.sort_by { |x| [-x.size, x[0], x[-1]] }.join("/")
+	
+end
+
+
+puts mix("my&friend&Paul has heavy hats!" , "my friend John has many many friends ")
+
+
+
